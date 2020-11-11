6dd03501a47e67b5ba9e594ff45348294ef1ca20
wuky <zz3692177@yahoo.com.tw>
2020-11-11 12:05:58 +0800

    feat: add delete function

--- app.js
+++ app.js
@@ -79,7 +79,15 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
+
